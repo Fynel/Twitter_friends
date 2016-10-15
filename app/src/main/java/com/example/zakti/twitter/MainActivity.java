1292b9717c7dc7fe7aae0604fb498e03024a0aa5
Fynel <zaktitus@gmail.com>
2016-10-15 09:49:58 -0400

    initial upload

---
+++ app/src/main/java/com/example/zakti/twitter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.zakti.twitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import twitter4j.Twitter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
